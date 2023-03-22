9c38839729033e486111e3fa5f9868e6a0fea51a
Hannes <hanneshogstrom02@gmail.com>
2023-03-22 14:23:39 +0100

    KeyListeners_exempel

---
+++ FingerTwister/Pressed button/src/Main.java
@@ -0,0 +1,11 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.addKeyListener;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Controller();
+    }
+}
